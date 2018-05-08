ef4ed8b0acc5dd82f9f44f84cf60bfedd4c292f9
Alexander Eremeev <alex.eremeev@web.de>
2018-05-08 23:03:09 +0200

    all

---
+++ src/tile/tile.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tictactoe-tile',
+  templateUrl: './tile.component.html',
+  styleUrls: ['./tile.component.scss']
+})
+export class TileComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+}
---
+++ src/tile/tile.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TileComponent } from './tile.component';
+
+@NgModule({
+  declarations: [TileComponent],
+  imports: [CommonModule],
+  exports: [TileComponent],
+  providers: []
+})
+export class TileModule {}
